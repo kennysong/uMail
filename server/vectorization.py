de64f226eecd2103c1b996a9734a75c829afb14d
Kenny Song <kenny.ysong@gmail.com>
2015-04-12 14:00:56 +0800

    Refactored vectorization.py

--- server/vectorization.py
+++ server/vectorization.py
@@ -8,16 +8,8 @@ import pickle
 from scipy.spatial import distance
 from external_modules import inflect
 
-def get_sentences_in_thread(thread):
-        emails = get_emails_in_thread(thread)
-
-        thread_sentences = []
-        for email in emails:
-                sentences = get_sentences_in_email(email)
-                for sentence in sentences:
-                        thread_sentences.append(sentence)
-
-        return thread_sentences
+def get_threads_in_root(root):
+        return list(root)
 
 def get_emails_in_thread(thread):
         emails = []
@@ -26,69 +18,28 @@ def get_emails_in_thread(thread):
                         emails.append(thread[i])
         return emails
 
-def get_sentences_in_email(email):
-        text_tag = None
-        for i in range(len(email)):
-                if email[i].tag == 'Text':
-                        text_tag = email[i]
-        return list(text_tag)
-
-def get_relative_position_in_email(sentence, emails):
-        for email in emails:
-                sentences = get_sentences_in_email(email)
-                if sentence in sentences:
-                        return sentences.index(sentence) / float(len(sentences)) * 100
-
-def clean_up_words(word_list):
-        # Make lowercase
-        clean_word_list = [word.lower() for word in word_list]
-
-        # Remove non alphabet or numbers
-        for i in range(len(clean_word_list)):
-                clean_word_list[i] = ''.join([t for t in clean_word_list[i] if (t in '1234567890' or t in string.ascii_lowercase)])
-
-        # Make singular
-        for i in range(len(clean_word_list)):
-                if inflect_engine.singular_noun(clean_word_list[i]) != False:
-                                clean_word_list[i] = inflect_engine.singular_noun(clean_word_list[i])
-
-        return clean_word_list
-
-def get_subject_similarity(subject, sentence):
-        # Split into lists of words
-        subject_words = clean_up_words(subject.text.split())
-        sentence_words = clean_up_words(sentence.text.split())
-
-        # Get # of words occurring in both
-        overlap = len(set(subject_words).intersection(set(sentence_words)))
-        return overlap
-
 def get_email_of_sentence(sentence, emails):
         for email in emails:
                 sentences = get_sentences_in_email(email)
                 if sentence in sentences:
                         return email
 
-def get_num_recipients(email):
-        num_recipients = 0
-        for email_tag in email:
-                if email_tag.tag == 'To' or email_tag.tag == 'Cc':
-                        if email_tag.text is None: continue
-                        num_recipients += email_tag.text.count('@')
-        return num_recipients
+def get_sentences_in_thread(thread):
+        emails = get_emails_in_thread(thread)
+        thread_sentences = []
+        for email in emails:
+                sentences = get_sentences_in_email(email)
+                for sentence in sentences:
+                        thread_sentences.append(sentence)
 
-def get_threads_in_root(root):
-        return list(root)
+        return thread_sentences
 
-def get_words_in_sentence(sentence):
-        return sentence.text.split()
-
-def get_words_in_email(email):
-        word_list = []
-        sentences = get_sentences_in_email(email)
-        for sentence in sentences:
-                word_list += get_words_in_sentence(sentence)
-        return word_list
+def get_sentences_in_email(email):
+        text_tag = None
+        for i in range(len(email)):
+                if email[i].tag == 'Text':
+                        text_tag = email[i]
+        return list(text_tag)
 
 def get_words_in_thread(thread):
         words = []
@@ -99,28 +50,77 @@ def get_words_in_thread(thread):
                         words += get_words_in_sentence(sentence)
         return words
 
-def get_tf_idf_vector(sentence, thread, root):
+def get_words_in_email(email):
+        word_list = []
+        sentences = get_sentences_in_email(email)
+        for sentence in sentences:
+                word_list += get_words_in_sentence(sentence)
+        return word_list
+
+def get_words_in_sentence(sentence):
+        return sentence.text.split()
+
+def clean_up_words(word_list):
+        # Make lowercase
+        clean_word_list = [word.lower() for word in word_list]
+
+        # Remove non alphabet or numbers
+        for i in range(len(clean_word_list)):
+                clean_word_list[i] = ''.join([t for t in clean_word_list[i] if (t in '1234567890' or t in string.ascii_lowercase)])
+
+        # Make singular
+        inflect_engine = inflect.engine()
+        for i in range(len(clean_word_list)):
+                if inflect_engine.singular_noun(clean_word_list[i]) != False:
+                                clean_word_list[i] = inflect_engine.singular_noun(clean_word_list[i])
+
+        return clean_word_list
+
+def get_num_recipients(email):
+        num_recipients = 0
+        for email_tag in email:
+                if email_tag.tag == 'To' or email_tag.tag == 'Cc':
+                        if email_tag.text is None: continue
+                        num_recipients += email_tag.text.count('@')
+        return num_recipients
+
+def get_relative_position_in_email(sentence, emails):
+        for email in emails:
+                sentences = get_sentences_in_email(email)
+                if sentence in sentences:
+                        return sentences.index(sentence) / float(len(sentences)) * 100
+
+def get_subject_similarity(subject, sentence):
+        # Split into lists of words
+        subject_words = clean_up_words(subject.text.split())
+        sentence_words = clean_up_words(sentence.text.split())
+
+        # Get # of words occurring in both
+        overlap = len(set(subject_words).intersection(set(sentence_words)))
+        return overlap
+
+def get_tf_idf_vector(sentence, thread, root, word_counts_per_thread, vocab_list_index):
         threads = get_threads_in_root(root)
         thread_words = clean_up_words(get_words_in_thread(thread))
         clean_sentence = clean_up_words(sentence.text.split())
 
         # Calculate term-frequency, number of times word appears in thread
-        tf_vector = np.zeros(len(cached_vocab_list_index)) # Global variable, see below
+        tf_vector = np.zeros(len(vocab_list_index)) # Global variable, see below
         for word in clean_sentence:
-                occurrences = cached_word_counts_per_thread[thread][word] # Global variable, see below
+                occurrences = word_counts_per_thread[thread][word] # Global variable, see below
                 tf = occurrences / float(len(thread_words))
-                word_index = cached_vocab_list_index[word]
+                word_index = vocab_list_index[word]
                 tf_vector[word_index] = tf
 
         # Calculate inverse document frequency, on # of threads in entire corpus that have the word
-        idf_vector = np.zeros(len(cached_vocab_list_index))
+        idf_vector = np.zeros(len(vocab_list_index))
         thread_words = [set(clean_up_words(get_words_in_thread(thread))) for thread in threads]
         for word in clean_sentence:
                 num_threads_with_word = 0.0
                 for thread_word_list in thread_words:
                         if word in thread_word_list: num_threads_with_word += 1
                 idf = math.log(len(threads) / num_threads_with_word)
-                word_index = cached_vocab_list_index[word]
+                word_index = vocab_list_index[word]
                 idf_vector[word_index] = idf
 
         # Get tf*idf vector
@@ -128,20 +128,20 @@ def get_tf_idf_vector(sentence, thread, root):
 
         return tf_idf_vector
 
-def get_tf_local_idf_vector(sentence, thread, root):
+def get_tf_local_idf_vector(sentence, thread, root, word_counts_per_thread, vocab_list_index):
         thread_words = clean_up_words(get_words_in_thread(thread))
         clean_sentence = clean_up_words(sentence.text.split())
 
         # Calculate term-frequency, number of times word appears in thread
-        tf_vector = np.zeros(len(cached_vocab_list_index)) # Global variable, see below
+        tf_vector = np.zeros(len(vocab_list_index)) # Global variable, see below
         for word in clean_sentence:
-                occurrences = cached_word_counts_per_thread[thread][word] # Global variable, see below
+                occurrences = word_counts_per_thread[thread][word] # Global variable, see below
                 tf = occurrences / float(len(thread_words))
-                word_index = cached_vocab_list_index[word]
+                word_index = vocab_list_index[word]
                 tf_vector[word_index] = tf
 
         # Calculate inverse document frequency, on # of emails in thread that have the word
-        idf_vector = np.zeros(len(cached_vocab_list_index))
+        idf_vector = np.zeros(len(vocab_list_index))
         emails = get_emails_in_thread(thread)
         email_words = [set(clean_up_words(get_words_in_email(email))) for email in emails]
         for word in clean_sentence:
@@ -149,7 +149,7 @@ def get_tf_local_idf_vector(sentence, thread, root):
                 for email_word_list in email_words:
                         if word in email_word_list: num_emails_with_word += 1
                 idf = math.log(len(emails) / num_emails_with_word)
-                word_index = cached_vocab_list_index[word]
+                word_index = vocab_list_index[word]
                 idf_vector[word_index] = idf
 
         # Get tf*idf vector
@@ -157,18 +157,17 @@ def get_tf_local_idf_vector(sentence, thread, root):
 
         return tf_idf_vector
 
-if __name__ == '__main__':
+def vectorize_training_data():
         ## Set up XML tree
         tree = ET.parse('bc3_corpus/bc3corpus.1.0/corpus.xml')
         root = tree.getroot()
-        inflect_engine = inflect.engine()
 
         ## Get word occurrence counts for each thread
-        cached_word_counts_per_thread = dict()
+        word_counts_per_thread = dict()
         for thread in root:
                 words = get_words_in_thread(thread)
                 word_counts = collections.Counter(words)
-                cached_word_counts_per_thread[thread] = word_counts
+                word_counts_per_thread[thread] = word_counts
         print("Finished getting word counts for each thread")
 
         ## Calculate vocab list for entire corpus
@@ -179,7 +178,7 @@ if __name__ == '__main__':
                         if item not in vocab_list_set: 
                                 vocab_list.append(item)
                                 vocab_list_set.add(item)
-        cached_vocab_list_index = {word: index for index, word in enumerate(vocab_list)}
+        vocab_list_index = {word: index for index, word in enumerate(vocab_list)}
 
         print("Finished constructing vocab list")
 
@@ -259,3 +258,8 @@ if __name__ == '__main__':
                         sentence_vectors_metadata.append((sentence_vector, {'id': sentence.attrib['id'], 'listno': thread_id}))
 
                 print("Vectorized a thread")
+
+        return sentence_vectors, sentence_vectors_metadata
+
+if __name__ == '__main__':
+        sentence_vectors, sentence_vectors_metadata = vectorize_training_data()
