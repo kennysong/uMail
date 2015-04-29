e461403b9aa15bee4c28f4a5214ca8601ef0eea5
Quan Vuong <quan.vuong@nyu.edu>
2015-04-29 09:29:16 +0800

    Added clarifying comments for ideal_summary.py & annotation_score_each_sentence.py & length_each_sentence.py

--- server/annotation_score_each_sentence.py
+++ server/annotation_score_each_sentence.py
@@ -3,19 +3,26 @@ import operator
 import pickle
 from data.length_each_sentence import *
 
+#global variable: summary
+    #summary is a dictionary with the format 
+    #[thread_listno] = {} (sub dictionary)
+    #for sub dictionary
+    #keys being the sentence ID and 
+    #values being the no. of annotators that pick this sentence
+
 tree = ET.parse("bc3_corpus/bc3corpus.1.0/annotation.xml")
 root = tree.getroot() 
 
 def return_sentence_ID(sentences):
-    #return the sentence ID contained in the <sentences> in corpus as a list
+    #return the sentence ID contained in the <sentences> tag in corpus as a list
 
     sentences_ID = [sent.attrib["id"] for sent in sentences]
 
     return sentences_ID
 
 def update_summary_with_sentenceID(summary, thread_listno, sentences_ID):
-    #update summary(type:dicti) with sentences_ID(type:list)
-    #update means: add the sentence into the summary if not present, if present score += 1
+    #update summary(type:dict) with sentences_ID(type:list)
+    #update means: add the sentence into the summary if not present, if present annotation += 1
 
     for ID in sentences_ID:
         if ID not in summary[thread_listno].keys():
@@ -25,7 +32,7 @@ def update_summary_with_sentenceID(summary, thread_listno, sentences_ID):
 
 def update_summary_with_annotation(summary, thread_listno, annotation):
     #update summary(type:dict) with <annotation>(type:tree)
-    #update means: add the sentence into the summary if not present, if present score += 1
+    #update means: add the sentence into the summary if not present, if present annotation += 1
 
     for sentences in annotation:
         if sentences.tag == "sentences":
@@ -33,17 +40,19 @@ def update_summary_with_annotation(summary, thread_listno, annotation):
     update_summary_with_sentenceID(summary,thread_listno,sentences_ID)
 
 def update_summary_with_each_thread(summary,thread):
-    #update summary(type:dic) with <thread>(type:tree)
+    #update summary(type:dict) with <thread>(type:tree)
 
+    #Determine the thread_listno of current thread
     for listno in thread:
         if listno.tag == "listno": thread_listno = listno.text
 
-    three_annotation = [branch for branch in thread if branch.tag == "annotation"] 
+    #update summary(type:dict) with each <annotation> in <thread>
     for annotation in thread:
         if annotation.tag == "annotation":
             update_summary_with_annotation(summary,thread_listno,annotation)
 
 def update_summary(summary):
+	#update summary(type:dict) with <root>
     for thread in root:
         for listno in thread:
             if listno.tag == "listno":
@@ -53,19 +62,15 @@ def update_summary(summary):
         update_summary_with_each_thread(summary,thread)
     
 if __name__ == '__main__':
-    #summary is a dictionary with the format 
-    #[thread_listno] = {} (sub dictionary)
-    #for sub dictionary
-    #keys being the sentence ID and 
-    #values being the no. of annotators that pick this sentence
     summary = dict()
 
+    #update summary with annotation score for each sentence
     update_summary(summary)
 
-    #normalization by sentence length
-
+    #normalization by sentence length (no. of words)
     for thread in summary:
         for sentence in summary[thread]:
             summary[thread][sentence] = (summary[thread][sentence])/(length_each_sentence[thread][sentence])
 
     print summary
+
--- server/ideal_summary.py
+++ server/ideal_summary.py
@@ -1,29 +1,41 @@
-#Create ideal summary 
 import operator
 from data.length_each_sentence_data import *
 from data.annotation_score_each_sentence_data import *
 
+#global variable
+	#summary (type:dict) with the format
+	#[thread_listno] = {} (sub dict)
+	#sub dict contains the sentence_ID of ideal summary of <thread> that has <thread_listno>
+
 def sort_dict_by_value(dict): 
-    #Sorted the dictionary by values and turn into a tuple
+    #Sorted a dictionary by values in descending order and turn into a tuple
     dict_sorted = sorted(dict.items(), key=operator.itemgetter(1), reverse = True)
 
     return dict_sorted
 
+def return_ideal_summary_with_length_limit(length_limit, sentences_sorted_by_importance, thread_listno):
+	#Return ideal summary of a <thread> with length limit of 30% of no. of words in original email 
+
+	ideal_summary_word_count = 0
+
+	ideal_summary = []
+
+	for sentence in sentences_sorted_by_importance:
+		ideal_summary.append(sentence[0])
+		ideal_summary_word_count += ideal_summary_word_count + length_each_sentence[thread_listno][sentence[0]]
+		if ideal_summary_word_count >= length_limit:
+			return ideal_summary
+
+
 def return_ideal_summary(summary, thread_listno):
     
     #length_limit is the length limit of ideal summary - 30% of original email by word count 
-    length_limit = sum(length_each_sentence[thread_listno].itervalues())/100*30
+    length_limit = sum(length_each_sentence[thread_listno].values())/100*30
 
+    #Sort each sentence in a <thread> according to annotation score
     sentences_sorted_by_importance = sort_dict_by_value(annotation_score_each_sentence[thread_listno])
 
-    ideal_summary_word_count = 0
-
-    ideal_summary = []
-    for sentence in sentences_sorted_by_importance:
-        ideal_summary.append(sentence[0])
-        ideal_summary_word_count += ideal_summary_word_count + length_each_sentence[thread_listno][sentence[0]]
-        if ideal_summary_word_count >= length_limit:
-            break
+    ideal_summary = return_ideal_summary_with_length_limit(length_limit, sentences_sorted_by_importance, thread_listno)
 
     ideal_summary = sorted(ideal_summary)        
 
